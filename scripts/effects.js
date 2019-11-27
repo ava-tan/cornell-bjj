b8154cb8415860eaa860e4aac27952297067d78c
Asher Lipman <aml449@cornell.edu>
2019-11-27 13:39:19 -0500

    added navigation responsiveness to calendar page

---
+++ scripts/effects.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    //navigation responsiveness//
+    var nav_open=0;
+    $("#hamburger").on("click", function(){
+        if (nav_open%2==0){
+            $("nav").removeClass("desktop");
+            $("header").css({
+                'position':'fixed',
+                'top':'0'
+            });
+        }
+        else{
+            $("nav").addClass("desktop");
+            $("header").css("position", "static");
+        }
+        nav_open++;
+    });
+});
